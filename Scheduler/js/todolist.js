6a118d583ed6bedefb1be3d4dc8e6e2ad90379e4
hustle-dev <dlwoabsdk@gmail.com>
2021-10-19 21:34:27 +0900

    feat: add todo as comment

--- Scheduler/js/todolist.js
+++ Scheduler/js/todolist.js
@@ -1,11 +1,14 @@
+// todos를 현재 로컬스토리지 기준 오늘 일자에 있는 todolist로 업데이트를 해야함.
 let todos = JSON.parse(localStorage.getItem('user')).abc.todolist['20211019'];
 
-const $popup = document.querySelector('.popup');
+const $yearMonth = document.querySelector('.year-month');
 const $newTodo = document.querySelector('.new-todo');
 const $todoList = document.querySelector('.todo-list');
-const $overlay = document.querySelector('.overlay');
 
 const render = () => {
+  // 이 부분에 현재 date 받아온거 넣으면 됨 (나중에) 현재는 하드코딩 '2021/10/19'
+  $yearMonth.textContent = '2021/10/19';
+
   $todoList.innerHTML = todos
     .map(
       ({ id, content, completed }) => `
@@ -22,6 +25,7 @@ const render = () => {
 
 const setTodo = newTodo => {
   todos = newTodo;
+  // 이 부분에서 로컬 스토리지에서 관리되고 있는 날짜의 todo를 관리해주어야 함
   render();
 };
 
@@ -42,7 +46,7 @@ const removeTodo = id => {
 window.addEventListener('DOMContentLoaded', render);
 
 $newTodo.onkeyup = e => {
-  if (e.key !== 'Enter' || e.target.value === '') return;
+  if (e.key !== 'Enter' || e.target.value.trim() === '') return;
 
   addTodo(e.target.value);
 
@@ -61,9 +65,27 @@ $todoList.onclick = e => {
   removeTodo(e.target.closest('li').dataset.id);
 };
 
+// 투두리스트와 관련 없는 부분 (밖으로 빼야할 부분)
+
+const $calendarDate = document.querySelector('.calendar-date');
+
+const $popup = document.querySelector('.popup');
+const $overlay = document.querySelector('.overlay');
+
+const displayPopup = () => {
+  $popup.style.display = 'block';
+  $overlay.style.display = 'block';
+};
+
 $overlay.onclick = () => {
   $popup.style.display = 'none';
   $overlay.style.display = 'none';
+};
+
+$calendarDate.onclick = e => {
+  if (e.target.classList.contains('not-current')) return;
+
+  displayPopup();
 };
 
 // console.log(JSON.parse(localStorage.getItem('user')).abc.todolist['20211019']);
