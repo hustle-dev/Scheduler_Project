1d90d9553f6b5fe03388ccbb6a4509dd78cf90b6
hustle-dev <dlwoabsdk@gmail.com>
2021-10-20 20:41:11 +0900

    feat: allTodos store when beforeunload event happens

--- Scheduler/js/calendar.js
+++ Scheduler/js/calendar.js
@@ -1,29 +1,23 @@
 import { userKey, userInfo } from './store.js';
 
-console.log(userInfo.todolist['2021-10-19']);
-
-// console.log(userKey === 'abc');
-
-// console.log(Object.keys(JSON.parse(localStorage.getItem('user'))[userKey].todolist));
-// console.log(JSON.parse(localStorage.getItem('user'))[userKey].todolist);
-
 let todos = [];
+const allTodos = userInfo.todolist;
 
 // DOM Nodes
-const $calendar = document.querySelector('.calendar');
+
 const $calendarDate = document.querySelector('.calendar-date');
 
 const $yearMonth = document.querySelector('.year-month');
 const $newTodo = document.querySelector('.new-todo');
 const $todoList = document.querySelector('.todo-list');
 
-const convertToRealMonth = (() => {
+const convertToRegularMonth = (() => {
   const monthStr = [
     'JAN',
     'FEB',
     'MAR',
     'APR',
-    'MAy',
+    'MAY',
     'JUN',
     'JUL',
     'AUG',
@@ -44,27 +38,6 @@ const convertToRealMonth = (() => {
   };
 })();
 
-// state
-
-// const userData = {
-//   userName: 'jimmy',
-//   // selectedDate: null,
-//   todolist: [
-//     { id: 3, content: '알고리즘', completed: true },
-//     { id: 2, content: '지식창고', completed: false },
-//     { id: 1, content: 'TS공부', completed: false }
-//   ]
-// };
-
-// const userData
-// Functions
-
-// date format 으로 바꾸기 (넘겨줄 데이터)
-const calendarForamt = () => {
-  const format = n => (n < 10 ? '0' + n : n + '');
-  return date => `${date.getFullYear()}-${format(date.getMonth() + 1)}-${format(date.getDate())}`;
-};
-
 // date 채우기 (첫째날, 마지막날 구해서 date 배열 채우기)
 const createMonthDate = (
   () => (length, obj, start) =>
@@ -74,59 +47,58 @@ const createMonthDate = (
 
 // selectedYear , selectedMonth -> 고치기
 let [selectedYear, selectedMonth] = [new Date().getFullYear(), new Date().getMonth()];
-// 이번달 정보
-// const getLastDayOfMonth = new Date(selectedYear, selectedMonth + 1, 0).getDate();
-
-// 저번달 정보
-const prevYear = selectedYear - !selectedMonth;
-const prevMonth = selectedMonth === 0 ? 11 : selectedMonth - 1;
-const getFirstDayOfMonth = new Date(selectedYear, selectedMonth, 1).getDay();
-const getLastDateMonth = new Date(selectedYear, selectedMonth + 1, 0).getDate();
-
-// 다음달 정보
-const nextYear = selectedYear + +(selectedMonth === 11);
-const nextMonth = selectedMonth === 11 ? 0 : selectedMonth + 1;
-const lengthOfNextMonth = 6 - new Date(selectedYear, selectedMonth + 1, 0).getDay();
-
-// 이거 활용할끄야
-// const getMonth = month => new Date(0, month).toLocaleString('en-us', { month: 'short' });
-const dataOfPrevMonth = {
-  year: prevYear,
-  month: convertToRealMonth.makeNum(prevMonth),
-  current: false
-};
-const dateOfThisMonth = {
-  year: selectedYear,
-  month: convertToRealMonth.makeNum(selectedMonth),
-  current: true
-};
-const dataOfNextMonth = {
-  year: nextYear,
-  month: convertToRealMonth.makeNum(nextMonth),
-  current: false
-};
-
-const mergeEachMonth = [
-  ...createMonthDate(getFirstDayOfMonth, dataOfPrevMonth, getLastDateMonth - getFirstDayOfMonth),
-  ...createMonthDate(getLastDateMonth, dateOfThisMonth, 1),
-  ...createMonthDate(lengthOfNextMonth, dataOfNextMonth, 1)
-];
 
 const render2 = () => {
+  const prevYear = selectedYear - !selectedMonth;
+  const prevMonth = selectedMonth === 0 ? 11 : selectedMonth - 1;
+  const firstDayOfMonth = new Date(selectedYear, selectedMonth, 1).getDay();
+  const lastDateMonth = new Date(selectedYear, selectedMonth + 1, 0).getDate();
+
+  const nextYear = selectedYear + +(selectedMonth === 11);
+  const nextMonth = selectedMonth === 11 ? 0 : selectedMonth + 1;
+  const lengthOfNextMonth = 6 - new Date(selectedYear, selectedMonth + 1, 0).getDay();
+
+  // 이거 활용할끄야
+  // const getMonth = month => new Date(0, month).toLocaleString('en-us', { month: 'short' });
+  const dateOfPrevMonth = {
+    year: prevYear,
+    month: convertToRegularMonth.makeNum(prevMonth),
+    current: false
+  };
+  const dateOfThisMonth = {
+    year: selectedYear,
+    month: convertToRegularMonth.makeNum(selectedMonth),
+    current: true
+  };
+  const dateOfNextMonth = {
+    year: nextYear,
+    month: convertToRegularMonth.makeNum(nextMonth),
+    current: false
+  };
+
+  const mergeEachMonth = [
+    ...createMonthDate(firstDayOfMonth, dateOfPrevMonth, lastDateMonth - firstDayOfMonth),
+    ...createMonthDate(lastDateMonth, dateOfThisMonth, 1),
+    ...createMonthDate(lengthOfNextMonth, dateOfNextMonth, 1)
+  ];
+
+  document.querySelector('.calendar-month').innerHTML = `${convertToRegularMonth.makeStr(
+    selectedMonth
+  )}<span>${selectedYear}</span>`;
   $calendarDate.innerHTML = mergeEachMonth
     .map(
       ({ year, month, date, current }) =>
         `<button class="${
           current ? '' : 'other-month'
-        }"><time datetime="${year}-${month}-${date}">${date}<div class="todo"></div></time></button>`
+        }"><time datetime="${year}-${month}-${date}">${date}<div class="todo"></div></time></time></button>`
     )
     .join('');
 
   $calendarDate.querySelectorAll('time').forEach(data => {
-    if (Object.keys(userInfo.todolist).includes(data.dateTime)) {
+    if (Object.keys(allTodos).includes(data.dateTime)) {
       data.firstElementChild.innerHTML = `<div class="todo-item">${
-        userInfo.todolist[data.dateTime][0].content
-      }</div><div class="todo-item">${userInfo.todolist[data.dateTime][1].content}</div>`;
+        allTodos[data.dateTime][0].content
+      }</div><div class="todo-item">${allTodos[data.dateTime][1].content}</div>`;
     }
   });
 };
@@ -134,17 +106,38 @@ const render2 = () => {
 // Event bindings --------------------------------------
 window.addEventListener('DOMContentLoaded', render2);
 
-// prev-button 누르면 앞으로, next-buttons 누르면 다음으로 이동
-$calendar.onclick = e => {
-  if (!e.target.matches('button')) return;
-  if (e.target.classList.contains('move-prev-months')) {
-    [selectedYear, selectedMonth] = [new Date().getFullYear(), new Date().getMonth() - 1];
-  }
+window.addEventListener('beforeunload', event => {
+  event.preventDefault();
 
-  if (e.target.classList.contains('move-next-months')) {
-    [selectedYear, selectedMonth] = [new Date().getFullYear(), new Date().getMonth() + 1];
-  }
+  localStorage.setItem(
+    'ho',
+    JSON.stringify({
+      name: 'home',
+      todolist: allTodos
+    })
+  );
+  event.returnValue = '';
+});
+
+document.querySelector('.move-prev-months').onclick = () => {
+  if (selectedMonth === 0) {
+    selectedMonth = 11;
+    selectedYear -= 1;
+  } else selectedMonth -= 1;
+
+  render2();
 };
+
+document.querySelector('.move-next-months').onclick = () => {
+  if (selectedMonth === 11) {
+    selectedMonth = 0;
+    selectedYear += 1;
+  } else selectedMonth += 1;
+
+  render2();
+};
+
+// ---------------------------------------------------------------------------------------------
 
 const $popup = document.querySelector('.popup');
 const $overlay = document.querySelector('.overlay');
@@ -154,9 +147,17 @@ const displayPopup = () => {
   $overlay.style.display = 'block';
 };
 
+const updateAllTodos = () => {
+  if (todos.length === 0) delete allTodos[`${document.querySelector('.year-month').textContent}`];
+  if (todos.length !== 0) allTodos[`${document.querySelector('.year-month').textContent}`] = todos;
+};
+
 $overlay.onclick = () => {
   $popup.style.display = 'none';
   $overlay.style.display = 'none';
+
+  updateAllTodos(todos);
+  render2();
 };
 
 const render = () => {
@@ -176,7 +177,6 @@ const render = () => {
 
 const setTodo = newTodo => {
   todos = newTodo;
-  console.log(todos);
   render();
 };
 
