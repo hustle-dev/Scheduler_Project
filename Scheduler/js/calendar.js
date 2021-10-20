a664bb8ddb8764889658b6c38bcc6248d60aa546
kkdd0757 <kkdd0757@hanmail.net>
2021-10-20 19:04:22 +0900

    refactor: simplify codes of calendar.js
    1. tried to simplify onclick event bindings

--- Scheduler/js/calendar.js
+++ Scheduler/js/calendar.js
@@ -1,7 +1,11 @@
 // DOM Nodes
-const $calendar = document.querySelector('.calendar');
+const $calendarDate = document.querySelector('.calendar-date');
+const $popup = document.querySelector('.popup');
+const $overlay = document.querySelector('.overlay');
 
-// state
+// Variables ------------------------------
+const selectedYear = new Date().getFullYear();
+let selectedMonth = new Date().getMonth();
 
 // const userData = {
 //   userName: 'jimmy',
@@ -13,30 +17,17 @@ const $calendar = document.querySelector('.calendar');
 //   ]
 // };
 
-// const userData
-// Functions
+// Functions------------------------------------------
 
-// date format 으로 바꾸기 (넘겨줄 데이터)
-const calendarForamt = () => {
-  const format = n => (n < 10 ? '0' + n : n + '');
-  return date => `${date.getFullYear()}-${format(date.getMonth() + 1)}-${format(date.getDate())}`;
-};
-
-// date 채우기 (첫째날, 마지막날 구해서 date 배열 채우기)
 const createMonthDate = (
   () => (length, obj, start) =>
     // eslint-disable-next-line no-param-reassign
     Array.from({ length }, () => ({ ...obj, date: start++ }))
 )();
 
-// selectedYear , selectedMonth -> 고치기
-let [selectedYear, selectedMonth] = [new Date().getFullYear(), new Date().getMonth()];
-// 이번달 정보
-// const getLastDayOfMonth = new Date(selectedYear, selectedMonth + 1, 0).getDate();
-
 const render2 = () => {
-  const convertToRealMonth = (() => {
-    const monthStr = [
+  const convertToRegularMonth = (() => {
+    const monthName = [
       'JAN',
       'FEB',
       'MAR',
@@ -54,50 +45,50 @@ const render2 = () => {
 
     return {
       makeStr(month) {
-        return monthStr[month];
+        return monthName[month];
       },
       makeNum(month) {
         return monthNum[month];
       }
     };
   })();
-  // 저번달 정보
+
   const prevYear = selectedYear - !selectedMonth;
   const prevMonth = selectedMonth === 0 ? 11 : selectedMonth - 1;
-  const getFirstDayOfMonth = new Date(selectedYear, selectedMonth, 1).getDay();
-  const getLastDateMonth = new Date(selectedYear, selectedMonth + 1, 0).getDate();
+  const firstDayOfMonth = new Date(selectedYear, selectedMonth, 1).getDay();
+  const lastDateMonth = new Date(selectedYear, selectedMonth + 1, 0).getDate();
 
-  // 다음달 정보
   const nextYear = selectedYear + +(selectedMonth === 11);
   const nextMonth = selectedMonth === 11 ? 0 : selectedMonth + 1;
   const lengthOfNextMonth = 6 - new Date(selectedYear, selectedMonth + 1, 0).getDay();
 
   // 이거 활용할끄야
   // const getMonth = month => new Date(0, month).toLocaleString('en-us', { month: 'short' });
-  const dataOfPrevMonth = {
+  const dateOfPrevMonth = {
     year: prevYear,
-    month: convertToRealMonth.makeNum(prevMonth),
+    month: convertToRegularMonth.makeNum(prevMonth),
     current: false
   };
   const dateOfThisMonth = {
     year: selectedYear,
-    month: convertToRealMonth.makeNum(selectedMonth),
+    month: convertToRegularMonth.makeNum(selectedMonth),
     current: true
   };
-  const dataOfNextMonth = {
+  const dateOfNextMonth = {
     year: nextYear,
-    month: convertToRealMonth.makeNum(nextMonth),
+    month: convertToRegularMonth.makeNum(nextMonth),
     current: false
   };
 
   const mergeEachMonth = [
-    ...createMonthDate(getFirstDayOfMonth, dataOfPrevMonth, getLastDateMonth - getFirstDayOfMonth),
-    ...createMonthDate(getLastDateMonth, dateOfThisMonth, 1),
-    ...createMonthDate(lengthOfNextMonth, dataOfNextMonth, 1)
+    ...createMonthDate(firstDayOfMonth, dateOfPrevMonth, lastDateMonth - firstDayOfMonth),
+    ...createMonthDate(lastDateMonth, dateOfThisMonth, 1),
+    ...createMonthDate(lengthOfNextMonth, dateOfNextMonth, 1)
   ];
-  document.querySelector('.calendar-month').innerHTML = `${convertToRealMonth.makeStr(
+
+  document.querySelector('.calendar-month').innerHTML = `${convertToRegularMonth.makeStr(
     selectedMonth
-  )}<span> ${selectedYear}</span>`;
+  )}<span>${selectedYear}</span>`;
   $calendarDate.innerHTML = mergeEachMonth
     .map(
       ({ year, month, date, current }) =>
@@ -111,36 +102,42 @@ const render2 = () => {
 // Event bindings --------------------------------------
 window.addEventListener('DOMContentLoaded', render2);
 
-// prev-button 누르면 앞으로, next-buttons 누르면 다음으로 이동
-$calendar.onclick = e => {
-  if (!e.target.matches('button')) return;
-  if (e.target.classList.contains('move-prev-months')) {
-    selectedMonth -= 1;
-    render2();
-  }
+// document.querySelector('.move-prev-months').onclick = () => {
+//   if (selectedMonth === 0) {
+//     selectedMonth = 11;
+//     selectedYear -= 1;
+//   } else selectedMonth -= 1;
 
-  if (e.target.classList.contains('move-next-months')) {
-    console.log('b');
-    selectedMonth += 1;
-    render2();
-  }
+//   render2();
+// };
+
+// document.querySelector('.move-next-months').onclick = () => {
+//   if (selectedMonth === 11) {
+//     selectedMonth = 0;
+//     selectedYear += 1;
+//   } else selectedMonth += 1;
+
+//   render2();
+// };
+
+document.querySelector('.calendar').onclick = () => {
+  // if (!e.target.matches('button')) return;
+  if (Math.abs(11 - selectedMonth) === 0 || Math.abs(11 - selectedMonth) === 11) {
+    selectedMonth = Math.abs(selectedMonth - 11);
+  } else selectedMonth += 1;
+  render2();
 };
-
-const $calendarDate = document.querySelector('.calendar-date');
-
-const $popup = document.querySelector('.popup');
-const $overlay = document.querySelector('.overlay');
 
 const displayPopup = () => {
   $popup.style.display = 'block';
   $overlay.style.display = 'block';
 };
 
+$calendarDate.onclick = () => {
+  displayPopup();
+};
+
 $overlay.onclick = () => {
   $popup.style.display = 'none';
   $overlay.style.display = 'none';
-};
-
-$calendarDate.onclick = () => {
-  displayPopup();
 };
